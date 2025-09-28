3712436ac496585ebe77f81ad6c9ec9f2faeb772
nareshungarala396 <nareshungarala220@gmail.com>
2025-09-28 17:54:58 +0530

    helloworld

--- 4.redis.sh
+++ 4.redis.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_NAME=$( echo $0 | cut -d "." -f2 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
 mkdir -p $LOGS_FOLDER
